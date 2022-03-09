eeff15efe8b9abd55c9faf279e1823ee6b4f83db
afrochonsp <afrochonsp@gmail.com>
2022-03-09 16:51:24 +0300

    Add new project

---
+++ BSUIR_Lesson1/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] float speed = 100;
+    float gravity = 9.8f;
+    Vector2 direction;
+    CharacterController characterController;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    public void SetMovementDirection(Vector2 direction)
+    {
+        this.direction = direction;
+    }
+
+    void Update()
+    {
+        Vector3 movement = Vector3.ClampMagnitude(new Vector3(direction.x, 0, direction.y) * speed, speed);
+        movement.y = -gravity;
+        movement = transform.TransformDirection(movement) * Time.deltaTime;
+        characterController.Move(movement);
+    }
+}
---
+++ BSUIR_Lesson1/Assets/Scripts/FirstPersonCharacter.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterMovement))]
+public class FirstPersonCharacter : MonoBehaviour
+{
+    [SerializeField] float mouseSensivity = 2;
+    [SerializeField] float cameraPitchRange = 180;
+    CharacterMovement characterMovement;
+    Camera _camera;
+    float cameraPitch;
+
+    void Start()
+    {
+        characterMovement = GetComponent<CharacterMovement>();
+        _camera = GetComponentInChildren<Camera>();
+        cameraPitch = _camera.transform.localEulerAngles.x;
+        HideCursor();
+    }
+
+    void Update()
+    {
+        characterMovement.SetMovementDirection(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+        CameraRotation();
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shot();
+        }
+    }
+
+    void Shot()
+    {
+        Vector3 point = new Vector3(_camera.pixelWidth / 2, _camera.pixelHeight / 2, 0);
+        Ray ray = _camera.ScreenPointToRay(point);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            GameObject hitObject = hit.transform.gameObject;
+            StartCoroutine(SphereIndicator(hit.point));
+        }
+    }
+
+    IEnumerator SphereIndicator(Vector3 position)
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.position = position;
+        yield return new WaitForSeconds(1);
+        Destroy(sphere);
+    }
+
+    void CameraRotation()
+    {
+        cameraPitch -= Input.GetAxis("Mouse Y") * mouseSensivity;
+        cameraPitch = Mathf.Clamp(cameraPitch, -cameraPitchRange / 2, cameraPitchRange / 2);
+        _camera.transform.localEulerAngles = new Vector3(cameraPitch, _camera.transform.localEulerAngles.y, _camera.transform.localEulerAngles.z);
+        transform.Rotate(0, Input.GetAxis("Mouse X") * mouseSensivity, 0);
+    }
+
+    void HideCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void ShowCusor()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    void OnGUI()
+    {
+        int size = 12;
+        float posX = _camera.pixelWidth / 2 - size / 4;
+        float posY = _camera.pixelHeight / 2 - size / 2;
+        GUI.contentColor = Color.black;
+        GUI.Label(new Rect(posX, posY, size, size), "*");
+    }
+}
+

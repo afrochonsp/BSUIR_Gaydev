d8198827357e2039975975e9efe9010fa493f2f3
Vanikiopik <vanikiopik@gmail.com>
2022-03-09 23:00:24 +0300

    ADD: create jumping script

--- BSUIR_Lesson1/Assets/Scripts/CharacterMovement.cs
+++ BSUIR_Lesson1/Assets/Scripts/CharacterMovement.cs
@@ -3,27 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(Rigidbody))]
 public class CharacterMovement : Character
 {
     [SerializeField] float speed = 100;
     float gravity = 9.8f;
     Vector2 direction;
     CharacterController characterController;
+    Rigidbody rigidbody;
+    Vector3 movement;
+    float movementY;
+
 
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        rigidbody = GetComponent<Rigidbody>();
     }
-
+    
     public void SetMovementDirection(Vector2 direction)
     {
         this.direction = direction;
     }
 
+    public void Jump()
+    {
+        if (characterController.isGrounded)
+        {
+            movementY = 120f;
+        }
+        
+    }
+
+
+
     void Update()
     {
-        Vector3 movement = Vector3.ClampMagnitude(new Vector3(direction.x, 0, direction.y) * speed, speed);
-        movement.y = -gravity;
+        movement = Vector3.ClampMagnitude(new Vector3(direction.x, movement.y, direction.y) * speed, speed);
+        if (characterController.isGrounded == false)
+        {
+        movementY -= gravity / 4;
+        }
+        movement.y = movementY;
         movement = transform.TransformDirection(movement) * Time.deltaTime;
         characterController.Move(movement);
     }
